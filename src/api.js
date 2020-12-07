efa7c2426a0be2e96c1d3e431f40e668323036a0
yash-sethia <56500374+yash-sethia@users.noreply.github.com>
2020-12-07 16:16:10 +0530

    Make CSAI better again

---
+++ src/api.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+export default {
+  user: {
+    login: (credentials) =>
+      axios.post("backend/login", { credentials }).then((res) => {
+        console.log("Hey", res.data.user);
+        return res.data.user;
+      }),
+
+    signup: (user) =>
+      axios.post("backend/signup", { user }).then((res) => {
+        console.log("Test", res.data.user);
+        return res.data.user;
+      }),
+  },
+  thread: {
+    createThread: (details) =>
+      axios.post("backend/newthread", { details })
+        .then((res) => {
+          console.log("The new Thread is as follows : ", res);
+          window.location.href="/forum";
+          return;
+        }),
+    editThread: (details) =>
+      axios
+        .put("/backend/editthread", { details })
+        .then((res) => res.data.thread),
+    deleteThread: (threadId) =>
+      axios
+        .delete(`/backend/deletethread/${threadId}`)
+        .then((res) => res.data.message),
+  },
+  comment: {
+    createComment: (details) =>
+      axios
+        .post("/backend/newcomment", { details })
+        .then(() => window.location.reload()),
+    editComment: (details) =>
+      axios
+        .put("/backend/editcomment", { details })
+        .then(() => console.log("Hello from api.js, I just added new comment")),
+    deleteComment: (commentId) =>
+      axios
+        .delete(`/backend/deletecomment/${commentId}`)
+        .then((res) => res.data.message),
+  },
+};
---
+++ src/components/pages/NewThreadPage.jsx
@@ -0,0 +1,48 @@
+import React,{Component} from 'react';
+import NewThreadForm from '../forms/NewThreadForm'
+import {connect} from 'react-redux'
+import {Segment} from 'semantic-ui-react'
+import PropTypes from 'prop-types';
+import axios from 'axios'
+import {createThread} from '../../actions/thread';
+import NavBar from '../misc/Navigation';
+
+class NewThreadPage extends Component{
+    constructor(props){
+        super(props)
+        this.submit = this.submit.bind(this);
+    }
+    submit = (data) =>{
+        return this.props.createThread(data)
+            .then(() => {
+                window.location.href="/forum";
+                // this.props.history.push("/forum")
+        });
+    }
+
+    // .then(() => {
+    //     window.location.href="/forum";
+    //     // this.props.history.push('/forum');
+    //     // return(this.props.toggleEdit());
+    //   })
+    render(){
+        return(
+            <div>
+                <NavBar />
+                <Segment style = {{background: "#ED4832"}}>
+                    <h2>Create a new thread</h2>
+                    <NewThreadForm submit={this.submit}/>
+                </Segment>
+            </div>
+        )
+
+    }
+}
+NewThreadPage.propTypes= {
+    history: PropTypes.shape({
+        push: PropTypes.func.isRequired
+    }).isRequired,
+    createThread: PropTypes.func.isRequired
+}
+
+export default connect(null, {createThread})(NewThreadPage)
